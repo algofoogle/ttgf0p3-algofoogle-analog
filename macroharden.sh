853630aeefe56a8014f2ae5574f7cfb9bf479ce7
Anton Maurovic <anton@foogle.com>
2026-06-30 22:19:22 +0930

    macroharden changes to support multiple digital blocks

--- macroharden.sh
+++ macroharden.sh
@@ -4,13 +4,23 @@ if [ -z "$VIRTUAL_ENV" ]; then echo "VENV is not loaded. Did you remember to run
 if [ -z "$PDK_ROOT"    ]; then echo "PDK_ROOT isn't set. Did you remember to run: source ./env-gf.sh"; exit 1; fi
 
 if [ -z "$1" ]; then
-    export HARDENCONFIG="librelane/digital/config.json"
+    echo "Usage: $0 MACRONAME [HARDENCONFIG]"
+    echo "Where:"
+    echo "  MACRONAME is the name of a librelane macro."
+    echo "  HARDENCONFIG is the path to a librelane config.json-style file"
+    exit 1
+fi
+
+export MACRONAME="$1"
+
+if [ -z "$2" ]; then
+    export HARDENCONFIG="librelane/$MACRONAME/config.json"
 else
     export HARDENCONFIG="$1"
 fi
 
-rm -rf librelane/runs/manual
-mkdir -p librelane/runs/manual
+rm -rf librelane/runs/$MACRONAME
+mkdir -p librelane/runs/$MACRONAME
 
 #NOTE: Chunks of this are informed by tt-support-tools, i.e. tt/project.py:harden()...
 python -m librelane \
@@ -18,8 +28,8 @@ python -m librelane \
     --dockerized \
     --pdk-root "$PDK_ROOT" \
     --pdk "$PDK" \
-    --run-tag manual \
-    --force-run-dir librelane/runs/manual \
+    --run-tag $MACRONAME \
+    --force-run-dir librelane/runs/$MACRONAME \
     "$HARDENCONFIG"
 #   --design-dir .
 
@@ -44,4 +54,4 @@ python -m librelane \
 #         --force-run-dir librelane/runs/manual \
 #         librelane/digital/config.json
 
-ls -aldh librelane/runs/manual/final/gds/*.gds
+ls -aldh librelane/runs/$MACRONAME/final/{gds/*.gds,pnl/*.v}
